09e3c984881d50fdff7a4faa5636cd5479c75c34
pbellot <pbellot@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-06 20:09:37 +0000

    - Lots of changes to fullfill BioCheck
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/netbench@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/grndata/inst/unitTests/test_grndata.R
@@ -0,0 +1,30 @@
+library(grndata)
+library(RUnit)
+#------------------------------------------------------------------------------
+runTests = function()
+{
+  test_DataSouces()
+  
+} # runTests
+#------------------------------------------------------------------------------
+test_DataSouces = function()
+{
+    datasources.names<-c("rogers1000","syntren1000","syntren300",
+    "gnw1565","gnw2000")
+    ngenes<-c(1000,1000,300,1565,2000)
+    nexp<-c(1000,1000,800,1565,2000)
+    edges<-c(1350,4695,468,7264,10392)
+    for(n in 1:5){
+        #loading the whole datasource
+        data<-eval(parse(text=paste(datasources.names[n],".data",sep=""))) 
+        #loading the golden standard
+        net<-eval(parse(text=paste(datasources.names[n],".net",sep="")))
+        checkEquals(is.data.frame(data),TRUE)
+        checkEquals(ncol(data), nrow(net))
+        checkEquals(ncol(data), ngenes[n])
+        checkEquals(nrow(data), nexp[n])
+        checkEquals(sum(net), edges[n])
+      checkEquals(sum(is.na(data)),0)
+  }
+} # test_DataSouces
+#------------------------------------------------------------------------------
