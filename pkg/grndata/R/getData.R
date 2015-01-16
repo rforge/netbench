0e6e374b5653ee48c2e5e831da7de37571fab15a
pbellot <pbellot@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-16 17:25:27 +0000

    - Adding function to easily load a datasource
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/netbench@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/grndata/R/getData.R
@@ -0,0 +1,19 @@
+getData <- function(datasource.name=NULL,getNet=TRUE){
+    Availabledata <- c("rogers1000","syntren1000","syntren300",
+        "gnw1565","gnw2000")
+    if(is.null(datasource.name)){
+        stop("Please specify a datasource name between the following ones:
+            rogers1000, syntren1000, syntren300, gnw1565 or gnw2000.")
+    }else{
+        if(!all(datasource.name %in% Availabledata)){
+            stop("The specified datasources are not available")
+        }
+        datasource <- eval(parse(text=paste(datasource.name,".data",sep="")))
+        if(getNet){
+            true.net <- eval(parse(text=paste(datasource.name,".net",sep="")))
+            list(datasource,true.net)
+        }else{
+            datasource
+        }
+    }
+}
